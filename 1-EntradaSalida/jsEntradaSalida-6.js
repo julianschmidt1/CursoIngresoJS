a06bf516d1c896e0522e25983b34bb868df8494f
alumno <alumno>
2019-02-06 20:25:01 -0300

    hasta el 7

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,18 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var primerNro;
+	var segundoNro;
+	var suma;
 
+	primerNro=numeroUno.value;
+	segundoNro=numeroDos.value;
+	
+	primerNro=parseInt(primerNro);
+	segundoNro=parseInt(segundoNro);
+
+	suma=primerNro+segundoNro;
+
+	alert("El resultado es: "+suma);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	var primerNro;
+	var segundoNro;
+	var suma;
+
+	primerNro=numeroUno.value;
+	segundoNro=numeroDos.value;
 	
+	primerNro=parseInt(primerNro);
+	segundoNro=parseInt(segundoNro);
+
+	suma=primerNro+segundoNro;
+
+	alert("El resultado es: "+suma);
 }
 
 function restar()
 {
+	var primerNro;
+	var segundoNro;
+	var resta;
+
+	primerNro=numeroUno.value;
+	segundoNro=numeroDos.value;
 	
+	primerNro=parseInt(primerNro);
+	segundoNro=parseInt(segundoNro);
+
+	resta=primerNro-segundoNro;
+
+	alert("El resultado es: "+resta);
 }
 
 function multiplicar()
 { 
+	var primerNro;
+	var segundoNro;
+	var multi;
+
+	primerNro=numeroUno.value;
+	segundoNro=numeroDos.value;
 	
+	primerNro=parseInt(primerNro);
+	segundoNro=parseInt(segundoNro);
+
+	multi=primerNro*segundoNro;
+
+	alert("El resultado es: "+multi);
 }
 
 function dividir()
 {
+	var primerNro;
+	var segundoNro;
+	var divi;
+
+	primerNro=numeroUno.value;
+	segundoNro=numeroDos.value;
 	
+	primerNro=parseInt(primerNro);
+	segundoNro=parseInt(segundoNro);
+
+	divi=primerNro/segundoNro;
+
+	alert("El resultado es: "+divi);
 }
 
