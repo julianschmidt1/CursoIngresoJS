10ec96f8c3c2cf0c1cde17c8a7d61e8b2c26c663
alumno <alumno>
2019-02-13 19:25:58 -0300

    ejerc 2 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,12 +1,20 @@
 function mostrar()
 {
-  	var nombre;
-  	var localidad;
+  	var precio;
+  	var recargo;
+  	var preciofinal;
 
-  	nombre=elNombre.value;
-  	localidad=laLocalidad.value;
+  	precio=elPrecio.value;
 
-  	alert("Usted es " +nombre+ " y vive en la localidad de " +localidad);
+  	precio=parseInt(precio);
+  	recargo=precio*10/100;
+  	preciofinal=precio+recargo;
 
+  	alert("El precio del producto en efectivo es de "+precio+". Con tarjeta tiene un recargo del 10% que seria "+recargo+", costando el final $"+preciofinal);
 
 }
+
+
+/*alert("el precio del productgo en efectivo es de :$ xxxx. 
+con tarjeta tiene n recargo del 10% que seria $xx de recargo,
+costando el final $xxxx")*/
