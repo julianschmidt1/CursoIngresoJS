ae83718398f9cab0f0e9e86601b99a692c085252
alumno <alumno>
2019-02-18 22:05:50 -0300

    while hasta el 3

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,9 @@ function mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
-
+while(clave!="utn750")
+{
+	alert("Error, la clave es incorrecta.");
+	clave=prompt("Ingrese la clave nuevamente.");
+}
 }//FIN DE LA FUNCIÓN
